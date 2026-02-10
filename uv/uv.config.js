ed8d3e5ee15fb0964744967fb2df0777f91459e5
Amber <148647402+theamb3r@users.noreply.github.com>
2026-02-10 11:17:12 -0800

    Add files via upload

---
+++ uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/char-service/',
+    bare:'https://bare.benrogo.net',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/static/uv/uv.sw.js',
+};
